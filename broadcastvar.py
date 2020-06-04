28e6ad69c85f8793cfb828a99560b504226525a3
Sripati Mishra <meetsripati@gmail.com>
2020-06-04 11:07:11 +0530

    Add files via upload

---
+++ broadcastvar.py
@@ -0,0 +1,16 @@
+from pyspark import SparkConf, SparkContext
+
+
+def loadMovieNames():
+    movieNames = {}
+    with open("c://SparkCourse/ml-100k/u.ITEM") as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
+conf = SparkConf().setMaster('local').setAppName('broadcast testing')
+sc = SparkContext(conf=conf)
+
+broadcastVar = sc.broadcast(loadMovieNames())
+
+print(broadcastVar.value)
